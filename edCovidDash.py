8b9495f69aaba773f57b48fca6643f4cd237f432
burke <jburke5@gmail.com>
2020-03-28 15:55:50 +0100

    adding simple mortaitly visualization

--- edCovidDash.py
+++ edCovidDash.py
@@ -5,6 +5,7 @@ from dash.dependencies import Input, Output
 
 import pandas as pd
 from uncertainties import ufloat
+import copy
 
 
 
@@ -17,30 +18,39 @@ mortalityDF = pd.read_csv('mortalityByAge.csv')
 mortalityDF.index=mortalityDF.ageCat
 mortalityDF = mortalityDF.drop(labels=mortalityDF.columns[0], axis='columns')
 
+baselineChartData = [{'x': mortalityDF.ageMedian, 'y': mortalityDF.caseFatality , 'type': 'scatter', 'name': 'Median Mortality for Age', 'mode' : 'markers', 'marker' : {'size':'10'}}]
+baselineChartLayout =  {'title': 'Age-Mortality Relationship', 'xaxis': {'title': 'Age', 'range':[30, 90]},'yaxis': {'title': 'Mortality', 'range': [-0.10, 1.0]}}
+
+
 app.layout = html.Div(children=[
     html.H1(children='ED Covid Risk Predictor'),
 
     html.Div(children=[
     	html.Div(children=[
 	    	html.Div(children=[html.Label('Age'), dcc.Input(type='number', value=65, id='ageInput')], id="agePanel"),
-    		html.Div(children=[html.Label('D-Dimer'), dcc.Input(type='number', value=0.0, id='dDimerInput')], id="dDimerPanel"),
-    		html.Div(children=[html.Label('IL-6'), dcc.Input(type='number', value=0.0, id='il6Input')], id="il6Panel"),
-		    html.Div(children=[html.Label('Ferritin'), dcc.Input(type='number', value=0.0, id='ferritinInput')], id="ferritinPanel"),
+    		html.Div(children=[html.Label('D-Dimer'), dcc.Input(type='number', value=1.0, id='dDimerInput')], id="dDimerPanel"),
+		    html.Div(children=[html.Label('Ferritin'), dcc.Input(type='number', value=50, id='ferritinInput')], id="ferritinPanel"),
 		], className='column', id="inputPanel", style={'width' : '15%'}),
 		html.Div(children=[html.Label('Mortality'), html.H3(id='MeanMortality'), html.H3(id='MortalityCI')], className='column', id="outcomeRisk", style={'width' : '25%'}),
+		html.Div(children=[
+			dcc.Graph(id='chartOfMortality',
+				figure={'data': baselineChartData,'layout': baselineChartLayout}
+			),
+	], className='column', id='figurePanel', style={'width':'50%'}),
+
 	], className='row', id='inputOutputContainer'),
 ], id="outermost")
 
 
 @app.callback(
     [Output(component_id='MeanMortality', component_property='children'),
-    Output(component_id='MortalityCI', component_property='children')],
+    Output(component_id='MortalityCI', component_property='children'),
+    Output(component_id='chartOfMortality', component_property='figure')],
     [Input(component_id='ageInput', component_property='value'),
     Input(component_id='dDimerInput', component_property='value'),
-    Input(component_id='il6Input', component_property='value'),
     Input(component_id='ferritinInput', component_property='value')]
 )
-def update_risks(age, dDimer, il6, ferritin):
+def update_risks(age, dDimer, ferritin):
 	cat = pd.cut(x=[age], bins=[0, 9, 19, 29, 39, 49, 59, 69, 79, 120])
 	ageMean = mortalityDF.iloc[cat.codes[0], 0]
 	ageSE = mortalityDF.iloc[cat.codes[0], 1]
@@ -54,11 +64,20 @@ def update_risks(age, dDimer, il6, ferritin):
 	ferritinCoeffs = [ufloat(1, 1), ufloat(9.1, 3.6) ]
 	ferritinOR = ferritinCoeffs[ferritinCat.codes[0]]
 	
-	il6OR = il6 * ufloat(1.12, 0.046)
-	totalBiomarkerOR = ddimerOR * ferritinOR * il6OR
+	#il6OR = il6 * ufloat(1.12, 0.046)
+	totalBiomarkerOR = ddimerOR * ferritinOR #* il6OR
 	combinedOdds = ageMortality/(1-ageMortality)*totalBiomarkerOR
 	combinedProb = combinedOdds/(combinedOdds+1)
-	return f"{combinedProb.nominal_value*100:.0f}%", f"95% CI [{(combinedProb.nominal_value-combinedProb.std_dev*1.96)*100:.0f}% - {(combinedProb.nominal_value+combinedProb.std_dev*1.96)*100:.0f}%]"
+	
+	newChartData = {'x':[age], 'y':[combinedProb.nominal_value], 'type' : 'scatter', 'mode' : 'marker', 'name' : 'Predicted Mortality for Characteristics', 'marker' : {'size' : 20}}
+	updatedData = copy.deepcopy(baselineChartData)
+	updatedData.append(newChartData)
+	figure = {'data' : updatedData, 'layout' : baselineChartLayout}
+	
+	mortalityString = f"{combinedProb.nominal_value*100:.0f}%"
+	ciString = f"95% CI [{(combinedProb.nominal_value-combinedProb.std_dev*1.96)*100:.0f}% - {(combinedProb.nominal_value+combinedProb.std_dev*1.96)*100:.0f}%]"
+	
+	return mortalityString, ciString, figure
 
 if __name__ == '__main__':
     app.run_server(debug=True)
